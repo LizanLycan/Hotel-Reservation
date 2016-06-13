ae4b5ef073a6cac39ccb99749869701499610622
LizanLycan <lizandroal.zerpa@gmail.com>
2016-06-13 09:27:32 -0400

    First Commit

---
+++ models/Reservations.js
@@ -0,0 +1,72 @@
+var keystone = require('keystone');
+var Types = keystone.Field.Types;
+
+/**
+ * Reservation Model
+ * =============
+ */
+
+var Reservation = new keystone.List('Reservation', {
+	nocreate: true,
+	noedit: true,
+});
+
+Reservation.add({
+	//client: { type: Types.Relationship, ref: 'User', index: true },
+	name: { type: Types.Name, required: true },
+	email: { type: Types.Email, required: true },
+	phone: { type: String },
+	dateIn: { type: Types.Date, required: true },
+	dateOut: { type: Types.Date, required: true },
+	roomCategory: { type: Types.Select, options: [
+		{ value: 'Sencillo', label: 'Habitacion sencilla con cama individual' },
+		{ value: 'Doble', label: 'Habitacion sencilla con dos camas individuales' },
+		{ value: 'Matrimonial', label: 'Habitacion sencilla con cama Matrimonial' },
+		{ value: 'Triple', label: 'Habitacion sencilla con cama Matrimonial y una individual'},
+	] },
+	comment: { type: Types.Markdown, required: true },
+	createdAt: { type: Date, default: Date.now },
+});
+
+Reservation.schema.pre('save', function(next) {
+	this.wasNew = this.isNew;
+	next();
+});
+
+Reservation.schema.post('save', function() {
+	if (this.wasNew) {
+		this.sendNotificationEmail();
+	}
+});
+
+Reservation.schema.methods.sendNotificationEmail = function(callback) {
+
+	if ('function' !== typeof callback) {
+		callback = function() {};
+	}
+
+	var Reservation = this;
+
+	keystone.list('User').model.find().where('isAdmin', true).exec(function(err, admins) {
+
+		if (err) return callback(err);
+
+    new keystone.Email({
+        
+        templateName: 'Reservation-notification'
+      }).send({
+        to: admins,
+        from: {
+          name: 'My Site',
+          email: 'contact@my-site.com'
+        },
+        subject: 'New Reservation for My Site',
+        enquiry: Reservation
+      }, callback);
+    });
+
+};
+
+Reservation.defaultSort = '-createdAt';
+Reservation.defaultColumns = 'clent, dateIn, dateOut, roomCategory, createdAt';
+Reservation.register();
---
+++ routes/views/reservation.js
@@ -0,0 +1,37 @@
+var keystone = require('keystone');
+var Reservation = keystone.list('Reservation');
+
+exports = module.exports = function (req, res) {
+
+	var view = new keystone.View(req, res);
+	var locals = res.locals;
+
+	// Set locals
+	locals.section = 'reservation';
+	locals.roomCategories = Reservation.fields.roomCategory.ops;
+	locals.formData = req.body || {};
+	locals.validationErrors = {};
+	locals.ReservationSubmitted = false;
+
+	// On POST requests, add the Reservation item to the database
+	view.on('post', { action: 'reservation' }, function (next) {
+
+		var newReservation = new Reservation.model();
+		var updater = newReservation.getUpdateHandler(req);
+
+		updater.process(req.body, {
+			flashErrors: true,
+			fields: 'name, email, phone, roomCategory, message',
+			errorMessage: 'There was a problem submitting your Reservation:',
+		}, function (err) {
+			if (err) {
+				locals.validationErrors = err.errors;
+			} else {
+				locals.ReservationSubmitted = true;
+			}
+			next();
+		});
+	});
+
+	view.render('reservation');
+};
